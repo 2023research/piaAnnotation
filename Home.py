10516b2e27af25bac13f2a2df13eb0ab9692c8ce
13706635 <shuming.liang@student.uts.edu.au>
2023-12-15 23:34:36 +1100

    decv15-0

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# http://13.211.130.199:8501/
+# http://13.211.130.199:8501/ 
 # python -m streamlit run Home.py
 from urllib.error import URLError
 import os, psycopg2, time
